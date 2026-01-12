e5289cd159e63734bf719b4b91fe66b9166f00d0
Omkar Kushwaha <137443710+omkarkushwaha@users.noreply.github.com>
2026-01-12 17:20:29 +0530

    Implement basic arithmetic operations in assign1.py

---
+++ assign1.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+print()
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+print(f'Addition: {addition}')
+print(f'Subtraction: {subtraction}')
+print(f'Multiplication: {multiplication}')
+print(f'Division: {division}')
